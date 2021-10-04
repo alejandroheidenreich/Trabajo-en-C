b16b8c1a777f7e79d5f0675adac63e891d5149a6
alejandroheidenreich <heidenreich.alejandro@gmail.com>
2021-10-04 08:39:35 -0300

    tp

--- TP_[2]/src/TP_[2].c
+++ TP_[2]/src/TP_[2].c
@@ -1,11 +1,25 @@
 /*
  ============================================================================
- Name        : TP_[2].c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+Una empresa requiere un sistema para administrar su nómina de empleados. Se sabe que
+dicha nómina bajo ninguna circunstancia superara los 1000 empleados.
+Datos:
+El sistema deberá tener el siguiente menú de opciones:
+1. ALTAS: Se debe permitir ingresar un empleado calculando automáticamente el número
+de Id. El resto de los campos se le pedirá al usuario.
+2. MODIFICAR: Se ingresará el Número de Id, permitiendo modificar: o Nombre o Apellido
+o Salario o Sector
+3. BAJA: Se ingresará el Número de Id y se eliminará el empleado del sistema.
+4. INFORMAR:
+1. Listado de los empleados ordenados alfabéticamente por Apellido y Sector.
+2. Total y promedio de los salarios, y cuántos empleados superan el salario promedio.
+NOTA: Se deberá realizar el menú de opciones y las validaciones a través de funciones.
+Tener en cuenta que no se podrá ingresar a los casos 2, 3 y 4; sin antes haber realizado la
+carga de algún empleado.
+Para la realización de este programa, se utilizará una biblioteca llamada “ArrayEmployees” que
+facilitará el manejo de la lista de empleados y su modificación. En la sección siguiente se
+detallan las funciones que esta biblioteca debe tener.
  ============================================================================
+ Alejandro Heidenreich
  */
 
 #include <stdio.h>
